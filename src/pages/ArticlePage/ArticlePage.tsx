6e73009aa0175cbabfa0ec2152c2abe6824e79bf
Nikita <shealtov@gmail.com>
2024-03-06 18:08:56 +0300

    Article page's in work

--- src/pages/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -9,26 +9,29 @@ type ArticleProps = {
 
 const ArticlePage: React.FC<ArticleProps> = (props) => {
   return (
-    <div className='article'>
-      <h1></h1>
+    <article className='article'>
+      <h1 className='article-heading'>Дэнни Вильнёв и его лучшие фильмы</h1>
       <img
-        className='article-left__background'
+        className='article-body__background'
         src='/assets/article-assets/1/villeneuve-cover.jpg'
         alt=''
       />
-      <div className='article-left'>
-        <p className='article-left__date'>{props.date}</p>
-        <div className='article-left__text'>
-          Последнее десятилетие Дени Вильнёв снимает легендарные фильмы, и
-          амбициозная "Дюна: Часть вторая" столь же политически радикальна и
-          масштабна, как и некоторые из его лучших работ. Вот наш рейтинг его
-          фильмов; вы можете следить за нашим подкастом, чтобы услышать
-          несколько исследований его эмоционально тщательных и концептуально
-          сложных аллегорий.
+      <div className='article-page'>
+        <div className='article-body'>
+          <p className='article-body__date'>{props.date}</p>
+          <p className='article-body__text'>
+            Последнее десятилетие Дени Вильнёв снимает легендарные фильмы, и
+            амбициозная "Дюна: Часть вторая" столь же политически радикальна и
+            масштабна, как и некоторые из его лучших работ. Вот наш рейтинг его
+            фильмов; вы можете следить за нашим подкастом, чтобы услышать
+            несколько исследований его эмоционально тщательных и концептуально
+            сложных аллегорий.
+          </p>
         </div>
         <div className='article-right'></div>
       </div>
-    </div>
+    </article>
   );
 };
+
 export default ArticlePage;
