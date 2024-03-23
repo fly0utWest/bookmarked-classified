e2fb3344e8e46d5d214453c44c62b707a0314c6a
Nikita <shealtov@gmail.com>
2024-03-23 10:09:25 +0300

    Centering login form finished

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -30,9 +30,9 @@ const LoginPage: React.FC = () => {
   return (
     <div className='login'>
       <div className='login__background'>
-        <img src='/assets/logo2.png' alt='' />
       </div>
       <div className='login-page'>
+        <img src='/assets/logo2.png' alt='' />
         <form className='login-form' onSubmit={handleSubmit}>
           <h1 className='login-form__heading'>Вход</h1>
           <p className='login-form__greeting'>
