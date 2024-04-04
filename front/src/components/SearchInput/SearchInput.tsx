95feac6e71e9b3195e2f826d3096b9690504f23b
Nikita <shealtov@gmail.com>
2024-04-04 22:16:16 +0300

    Saerch algorithm on frontend ready

--- front/src/components/SearchInput/SearchInput.tsx
+++ front/src/components/SearchInput/SearchInput.tsx
@@ -1,16 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { SearchInputProps } from '../../types';
+import { debounce } from '../../utils/debounce';
 
-const SearchInput: React.FC = () => {
+const SearchInput: React.FC<SearchInputProps> = ({onSearch}) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [input, setInput] = useState<string>('');
+
+  const debouncedSearch = debounce(onSearch, 1000);
+
 
   useEffect(() => {
     inputRef?.current?.focus();
   }, []);
 
+  useEffect(() =>{
+    if (input.trim()) {
+      debouncedSearch(input);
+    }
+  }, [input])
+
   return (
     <input
       className='search-input'
       type='search'
+      value={input}
+      onChange={(event) => setInput(event.target.value)}
       placeholder='Начните вводить название фильма...'
       ref={inputRef}
     />
